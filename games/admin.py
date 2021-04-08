007158824e90781dad5446704fd0a1baea6876c4
TunaWithSalmon <browsgaming5000@gmail.com>
2021-04-08 21:01:25 +0200

    Added migrations

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from games.models import Game, Package, Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['title',  ]
+
+
+class GameAdmin(admin.ModelAdmin):
+    model = Game
+    list_display = ['title', ]
+
+
+class PackageAdmin(admin.ModelAdmin):
+    model = Package
+    list_display = ['get_game', 'version', ]
+
+    def get_game(self, obj):
+        return obj.game.title
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Package, PackageAdmin)
--- games/models.py
+++ games/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from taggit.managers import TaggableManager
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=60, blank=False, null=False)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
 
 
 class Game(models.Model):
@@ -7,19 +22,31 @@ class Game(models.Model):
     cover_image = models.ImageField(blank=True, null=True)
     published = models.BooleanField()
     featured = models.BooleanField()
+    slug = models.SlugField()
     seo_title = models.CharField(max_length=60, blank=False, null=False)
     seo_description = models.CharField(max_length=150, blank=False, null=False)
     likes = models.IntegerField()
     dislikes = models.IntegerField()
     plays = models.IntegerField()
+    tags = TaggableManager()
+    category = models.ForeignKey(Category, blank=False, null=False, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('game_detail', kwargs={'slug': self.slug})
 
 
 class Package(models.Model):
-    title = models.CharField(blank=False, null=False)
+    title = models.CharField(max_length=60, blank=False, null=False)
     version = models.CharField(max_length=20, blank=False, null=False)
     published = models.BooleanField()
-    game = models.ForeignKey(Game, blank=False, null=False)
+    game = models.ForeignKey(Game, blank=False, null=False, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.game.title + " " + self.version
