0137b3b0ee5311e03fa326aafb42241d3749033a
TunaWithSalmon <browsgaming5000@gmail.com>
2021-04-08 19:16:51 +0200

    Added requirements.txt

--- games/models.py
+++ games/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=60, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    cover_image = models.ImageField(blank=True, null=True)
+    published = models.BooleanField()
+    featured = models.BooleanField()
+    seo_title = models.CharField(max_length=60, blank=False, null=False)
+    seo_description = models.CharField(max_length=150, blank=False, null=False)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+    plays = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Package(models.Model):
+    title = models.CharField(blank=False, null=False)
+    version = models.CharField(max_length=20, blank=False, null=False)
+    published = models.BooleanField()
+    game = models.ForeignKey(Game, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
