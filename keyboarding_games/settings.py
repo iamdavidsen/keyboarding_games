6839634c7382dbd31ee8d3cff9c7420b6abaff92
TunaWithSalmon <browsgaming5000@gmail.com>
2021-04-09 15:31:28 +0200

    Added env

--- keyboarding_games/settings.py
+++ keyboarding_games/settings.py
@@ -9,25 +9,25 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import environ
 import mimetypes
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env()
+
+DEBUG = env("DEBUG")
+
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "keyboarding-games.com"]
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=+(c@093%898r3_n7u)4ykqtv(5nc1soejw0==771mk_u*v4q8'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -77,17 +77,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'keyboarding_games.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': env.db('SQLITE_URL', default='sqlite:////tmp/my-tmp-sqlite.db')
     }
-}
-
+else:
+    DATABASES = {
+        'default': env.db(),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -106,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -147,5 +143,3 @@ mimetypes.add_type("text/html", ".html", True)
 mimetypes.add_type("text/javascript", ".js", True)
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
-
-
