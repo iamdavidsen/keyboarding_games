e64f9e927a90b8f94757c58c089f21b8a08b3781
TunaWithSalmon <browsgaming5000@gmail.com>
2021-04-12 07:16:25 +0200

    Added styles

--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,7 @@ from games.models import Game
 
 
 def home_view(request):
+    featured_games = Game.objects.filter(published=True, featured=True)
     games = Game.objects.filter(published=True)
 
-    return render(request, "pages/home.html", { 'games': games })
+    return render(request, "pages/home.html", { 'featured_games': featured_games, 'games': games })
