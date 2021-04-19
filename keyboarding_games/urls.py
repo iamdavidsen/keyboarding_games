5b9dcffc053970aefb473090c72a59889c684618
TunaWithSalmon <browsgaming5000@gmail.com>
2021-04-19 21:35:55 +0200

    Added robots.txt

--- keyboarding_games/urls.py
+++ keyboarding_games/urls.py
@@ -17,10 +17,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('pages.urls')),
-    path('games/', include('games.urls')),
-    path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
+                  path('', include('pages.urls')),
+                  path('games/', include('games.urls')),
+                  path('admin/', admin.site.urls),
+                  path(
+                      "robots.txt",
+                      TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+                  ),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                                            document_root=settings.MEDIA_ROOT)
