d243dfd121d732b21b60c62ae4ef6770360137c7
TunaWithSalmon <browsgaming5000@gmail.com>
2021-05-13 14:33:44 +0200

    Fix

--- games/views.py
+++ games/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from games.models import Package
 
 
 def game_view(request, slug):
-    package = Package.objects.select_related("game").filter(game__slug=slug).order_by('-id')[0]
+    packages = Package.objects.select_related("game").filter(game__slug=slug).order_by('-id')
 
-    return render(request, "games/game.html", { "package": package })
+    if packages.count() > 0:
+        return render(request, "games/game.html", { "package": packages[0] })
+
+    return redirect('home', permanent=True)
+
+
